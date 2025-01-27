8b001cb8c448b78cbca44ab3014e5c241fe48de7
Andrii <111363234+malandrii@users.noreply.github.com>
2025-01-26 21:15:28 -0500

    Javadoc comments added

--- Project 1/src/Line.java
+++ Project 1/src/Line.java
@@ -4,12 +4,33 @@
 import java.awt.Graphics;
 import java.awt.Point;
 
+/**
+ * Class representing a Line.
+ * The line is drawn between two points.
+ *
+ * @author Andrii Malakhovtsev
+ * @version 1.0
+ */
+
 public class Line extends Shape {
 
+    /**
+     * Constructs a Line object with two points and a color.
+     *
+     * @param p1 the first point representing the start of the line.
+     * @param p2 the second point representing the end of the line.
+     * @param color the color of the line.
+     */
     public Line(Point p1, Point p2, double color) {
         super(p1, p2, color);
     }
 
+    /**
+     * Draws the line on the given Graphics object.
+     * The line is drawn between the two points p1 and p2.
+     *
+     * @param g the Graphics object used for drawing.
+     */
     @Override
     public void draw(Graphics g) {
         // Line drawing logic (left empty for now)
--- Project 1/src/LineTest.java
+++ Project 1/src/LineTest.java
@@ -5,7 +5,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.awt.Point;
 
+/**
+ * Unit test for the Line class.
+ * This test verifies the constructor and getter methods of the Line class.
+ *
+ * @author Andrii Malakhovtsev
+ * @version 1.0
+ */
+
 public class LineTest {
+
+    /**
+     * Tests the constructor of the Line class.
+     * Verifies that the two points and color are correctly set in the Line object.
+     *
+     * @param p1 the first point that defines the start of the line.
+     * @param p2 the second point that defines the end of the line.
+     * @param color the color of the line.
+     */
 
     @Test
     public void testLineConstructor() {
