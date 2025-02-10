3510cfbde79e82c19387554ba2bfcdb1368a8848
Andrii <111363234+malandrii@users.noreply.github.com>
2025-02-09 20:39:04 -0500

    Project 3

---
+++ Project 3/src/main/java/DrawingApp.java
@@ -0,0 +1,32 @@
+// Andrii Malakhovtsev
+// Shapes, Phase 2
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class DrawingApp extends JFrame {
+    public static int WIDTH = 800;
+    public static int HEIGHT = 600;
+
+    private DrawingCanvas canvas;
+
+    public DrawingApp() {
+        canvas = new DrawingCanvas();
+        add(canvas);
+        setTitle("Drawing Program - Shapes, Phase 2");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                canvas.processKey(e.getKeyChar());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new DrawingApp();
+    }
+}
---
+++ Project 3/src/main/java/Shapes/ShapeType.java
@@ -0,0 +1,8 @@
+// Andrii Malakhovtsev
+// Shapes, Phase 2
+
+package Shapes;
+
+public enum ShapeType {
+    LINE, BOX, OVAL
+}
